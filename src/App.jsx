cca0344264107ec7c683df5a13572bbcc2cc8c5d
arnoldkajibwami <95924114+arnoldkajibwami@users.noreply.github.com>
2024-07-05 13:19:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Router>
-     { <Header/>
+     <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/home" element={<Home />} />
